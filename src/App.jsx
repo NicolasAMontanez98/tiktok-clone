14f2cb8a6719f719cbb02da709eab0882a135afe
NicolasAMontanez98 <nicomontanez98@gmail.com>
2023-02-21 10:29:58 -0500

    feat(Root): Start project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <main>
+        TikTok clone
+      </main>
+    </div>
+  )
+}
+
+export default App
